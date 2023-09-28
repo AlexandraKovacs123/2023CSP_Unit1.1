e9b45cd4af2efe814003c814d35e3d861635964e
Alexandra <alexandra.kovacs@vtsd.com>
2023-09-28 08:08:35 -0400

    h

---
+++ 1-1-3/step18.py
@@ -0,0 +1,40 @@
+#   a113_tower.py
+#   Modify this code in VS Code to alternate the colors of the
+#   floors every three floors
+import turtle as trtl
+
+painter = trtl.Turtle()
+painter.speed(0)
+painter.pensize(5)
+
+# starting location of the tower
+x = -150
+y = -150
+
+# height of tower and a counter for each floor
+num_floors = 63
+
+# iterate
+for floor in range(num_floors):
+    # set placement and color of turtle
+    painter.penup()
+    painter.goto(x, y)
+    painter.color("gray")
+    y = y + 5  # location of next floor
+
+if (rem > 3)
+
+rem= floor % 21
+if (rem == 0):
+    x = x + 60
+    y = 0
+
+painter.goto(x,y)
+y = y + 5
+
+    # draw the floor
+    painter.pendown()
+    painter.forward(50)
+
+wn = trtl.Screen()
+wn.mainloop()
