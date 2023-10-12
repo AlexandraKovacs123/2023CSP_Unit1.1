3c6f52d9587c79e3602760f3ed28d44bd97144db
Alexandra <alexandra.kovacs@vtsd.com>
2023-10-12 07:58:10 -0400

    h

--- 1-1-6/step5.py
+++ 1-1-6/step5.py
@@ -9,27 +9,43 @@ spider.pensize(40)
 spider.circle(20)
 width = 8
 
-#configure the spider legs
+# configure the spider legs
 lengthOfLegs = 70
-distanceOfLegs = 360 / width
+distanceOfLegs = 360 / width - 20
 spider.pensize(5)
 n = 0
 
 # draw the legs
 while (n < width):
+  spider.goto(0, 20)
   if (n < 4):
-    spider.goto(0,20)
     spider.setheading(distanceOfLegs * n - 45)
     spider.forward(lengthOfLegs)
-    n = n + 1
 
   else:
-    spider.goto(0, 20)
     spider.setheading(distanceOfLegs * n + 45)
     spider.forward(lengthOfLegs)
-    n = n + 1
+  n = n + 1
 
- # n = n + 1
+n = 0
+
+while (n < width):
+  spider.pensize(3)
+  if (n < 4):
+    spider.goto(5, 20)
+    spider.color("white")
+    spider.circle(5)
+
+  else:
+    spider.penup()
+    spider.goto(-15,20)
+    spider.color("white")
+    spider.pendown()
+    spider.circle(5)
+
+  n = n + 1
+
+
 spider.hideturtle()
 wn = trtl.Screen()
 wn.mainloop()
