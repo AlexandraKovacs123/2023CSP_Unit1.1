c12fae9511a888e41445fd9f605376bca98862bc
Alexandra <alexandra.kovacs@vtsd.com>
2023-11-02 09:24:13 -0400

    h

--- 1-1-9/project.py
+++ 1-1-9/project.py
@@ -1,4 +1,96 @@
 import turtle as trtl
 
+trtl.penup()
+trtl.goto(-50,200)
+trtl.fillcolor("white")
+trtl.speed(8)
+trtl.pendown()
+
+# draw that guy 2
+trtl.forward(150)
+trtl.right(90)
+trtl.forward(55)
+trtl.left(90)
+trtl.forward(15)
+trtl.right(90)
+trtl.forward(15)
+trtl.right(90)
+trtl.forward(15)
+trtl.left(90)
+trtl.forward(85)
+trtl.left(90)
+trtl.forward(60)
+trtl.right(90)
+trtl.forward(65)
+trtl.right(90)
+trtl.forward(60)
+trtl.left(90)
+trtl.forward(15)
+trtl.left(90)
+trtl.forward(40)
+trtl.right(90)
+trtl.forward(60)
+trtl.right(90)
+trtl.forward(85)
+trtl.right(90)
+trtl.forward(60)
+trtl.left(90)
+trtl.forward(15)
+trtl.left(90)
+trtl.forward(15)
+trtl.right(90)
+trtl.forward(70)
+trtl.left(90)
+trtl.forward(45)
+trtl.right(90)
+trtl.forward(70)
+trtl.right(90)
+trtl.forward(50)
+trtl.right(90)
+trtl.forward(30)
+trtl.left(90)
+trtl.forward(15)
+trtl.left(90)
+trtl.forward(15)
+trtl.right(90)
+trtl.forward(40)
+trtl.left(90)
+trtl.forward(50)
+trtl.right(90)
+trtl.forward(60)
+trtl.right(90)
+trtl.forward(60)
+trtl.left(90)
+trtl.forward(15)
+trtl.right(90)
+trtl.forward(15)
+trtl.left(90)
+trtl.forward(40)
+trtl.left(90)
+trtl.forward(15)
+trtl.right(90)
+trtl.forward(15)
+trtl.right(90)
+trtl.forward(15)
+trtl.left(90)
+trtl.forward(60)
+trtl.right(90)
+trtl.forward(10)
+
+"""
+import turtle as trtl
+penup
+goto somewhere in the middle of screen
+set turtle size and color
+pendown
+draw that guy #2
+move somewhere else
+make loop to
+    draw that guy #1
+    move somewhere else
+draw snow flakes using loop
+"""
+
+
 wn = trtl.Screen()
 wn.mainloop()
