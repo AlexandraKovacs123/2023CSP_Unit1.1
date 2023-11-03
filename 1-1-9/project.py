f1a050766d149bca61477d14ade5aa9e7ace0c33
Alexandra <alexandra.kovacs@vtsd.com>
2023-11-03 08:41:25 -0400

    h

--- 1-1-9/project.py
+++ 1-1-9/project.py
@@ -3,8 +3,11 @@ import turtle as trtl
 trtl.penup()
 trtl.goto(-50,200)
 trtl.fillcolor("white")
-trtl.speed(8)
+trtl.speed(12)
 trtl.pendown()
+
+# draw snowflakes
+
 
 # draw that guy 2
 trtl.forward(150)
@@ -76,6 +79,40 @@ trtl.left(90)
 trtl.forward(60)
 trtl.right(90)
 trtl.forward(10)
+
+# Starting location of eyes and color
+trtl.penup()
+trtl.fillcolor("black")
+trtl.forward(10)
+trtl.right(90)
+trtl.forward(90)
+trtl.left(90)
+trtl.penup()
+trtl.forward(20)
+
+trtl.pendown()
+
+# Draw accessories of guy 2
+for eyes in range (2):
+    trtl.shape("square")
+    # trtl.forward(40)
+    trtl.turtlesize(2)
+    trtl.stamp()
+    trtl.penup()
+    trtl.forward(80)
+    trtl.pendown()
+for smallEyes in range(2):
+    trtl.turtlesize(1)
+    trtl.penup()
+    trtl.fillcolor("white")
+    trtl.goto(-15,110)
+    trtl.stamp()
+    trtl.forward(80)
+
+for bow in range(2):
+    trtl.penup()
+    trtl.goto(-15,110)
+
 
 """
 import turtle as trtl
